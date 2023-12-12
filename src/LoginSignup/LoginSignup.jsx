ce1019f588ba23e8b4abdbd30a41e7b727655985
vi-n-a-y <balavinay9063@gmail.com>
2023-12-12 21:57:26 +0530

    login Page

---
+++ src/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './LoginSignup.css'
+import { useState } from 'react'
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+export const LoginSignup = () => {
+    const[action,setAction]=useState("Login");
+  return (
+    <div className="container">
+        <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+
+        </div>
+        <div className="inputs">
+            {action=="Login"?<div></div>:<div className="input">
+                <img src={user_icon} alt="user icon" />
+                <input type="text" placeholder="Name"/>
+            </div>}
+            
+            <div className="input">
+                <img src={email_icon} alt="email icon" />
+                <input type="email" placeholder="email"/>
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="password icon" />
+                <input type="password"  placeholder="password"/>
+            </div>
+            </div>
+            {action==="Sign Up"?<div></div>:<div className="forgot-password">forgot password ?<span>Click Here</span></div>}
+           <div className="submit-container">
+            <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+            <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>         
+        </div>
+    </div>
+ 
+  )
+}
